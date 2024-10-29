2b9d6983062aefd0f1afd98ddd4c5258e5246b51
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-10-29 18:26:51 +0300

    changed the name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 const Header = () => {
     return (
         <header className="bg-blue-600 text-white p-4 flex justify-between">
-            <h1 className="text-xl font-bold">AliExpress Affiliate Store</h1>
+            <h1 className="text-xl font-bold">Oroovis Shop</h1>
 
             <nav>
                 <ul className="flex space-x-4">
